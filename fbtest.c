321b1061ccf1cf0c95dcbf0e5fc705611ed15c71
Taiten Peng <taiten.peng@canonical.com>
2019-07-01 14:25:25 +0800

    update

--- fbtest.c
+++ fbtest.c
@@ -93,7 +93,6 @@ void fill_screen(void *fbmem,int HEIGHT,int WIDTH)
 #if 1 
 
 draw_pixel(fbmem,x,y,0xffff0000);
-printf("Drawing Height %d, Width= %d\n", HEIGHT,WIDTH);
 
 //draw_pixel(fbmem,x,y+HEIGHT,0xff00ff00);
 
